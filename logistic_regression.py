f62103e162dfa5e902bb8f39a85c4dcee8e41d08
Bruno Varella Peixoto <varellap@gmail.com>
2017-10-17 00:34:02 -0200

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,23 +1,25 @@
-import pandas
+#import pandas
+import numpy
 from keras.models import Sequential
 from keras.layers import Dense, Activation
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 
 # Dataset path
 DATASET_PATH = "/Users/bvarella/Google Drive/Algotrading NEW/Algotrading/ABEV3201701.csv"
 
 # Load dataset
-stocks_data = pandas.read_csv(DATASET_PATH)
-used_features = 'fechamento_anterior'
+#stocks_data = pandas.read_csv(DATASET_PATH)
+stocks_data = numpy.loadtxt(DATASET_PATH, delimiter=',', skiprows = 1, usecols = 1)
+#used_features = 'fechamento_atual'
 
 # Splitting data
-train_x, test_x, train_y, test_y = train_test_split(stocks_data[used_features], stocks_data[used_features], train_size = 0.7)
+train_x, test_x, train_y, test_y = train_test_split(stocks_data, stocks_data, train_size = 0.7, test_size = 0.3)
 
 # 2-class logistic regression
 model = Sequential()
-model.add(Dense(1, activation = 'sigmoid', input_shape = test_x.shape[1]))
-model.compile(optimizer = 'sgd', loss = 'binary_crossentropy', metrics = ['accuracy'])
-history = model.fit(test_x, test_y, nb_epoch = 10, validation_data = (train_x, train_y))
+model.add(Dense(1, activation = 'sigmoid', input_shape = numpy.shape(test_x)))
+model.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = ['accuracy'])
+history = model.fit(test_x, test_y, epochs = 10, validation_data = (train_x, train_y))
 score = model.evaluate(test_x, test_y, verbose = 0)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
