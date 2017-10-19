da2f8ccbc5383501401dbe906f6af6341186238f
Bruno Varella Peixoto <varellap@gmail.com>
2017-10-19 11:53:52 -0200

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,4 +1,3 @@
-#import pandas
 import numpy
 from keras.models import Sequential
 from keras.layers import Dense, Activation
@@ -8,16 +7,23 @@ from sklearn.model_selection import train_test_split
 DATASET_PATH = "/Users/bvarella/Google Drive/Algotrading NEW/Algotrading/ABEV3201701.csv"
 
 # Load dataset
-#stocks_data = pandas.read_csv(DATASET_PATH)
 stocks_data = numpy.loadtxt(DATASET_PATH, delimiter=',', skiprows = 1, usecols = 1)
-#used_features = 'fechamento_atual'
+
+# Classifying dataset
+data_classes = []
+for x in range (stocks_data.size):
+    if x+1 > x:
+        data_classes.append(1)
+    else:
+        data_classes.append(0)
 
 # Splitting data
-train_x, test_x, train_y, test_y = train_test_split(stocks_data, stocks_data, train_size = 0.7, test_size = 0.3)
+train_x, test_x, train_y, test_y = train_test_split(data_classes, data_classes, train_size = 0.7, test_size = 0.3)
+
 
 # 2-class logistic regression
 model = Sequential()
-model.add(Dense(1, activation = 'sigmoid', input_shape = numpy.shape(test_x)))
+model.add(Dense(1, activation = 'sigmoid', input_dim = 1))
 model.compile(optimizer = 'sgd', loss = 'binary_crossentropy', metrics = ['accuracy'])
 history = model.fit(test_x, test_y, epochs = 10, validation_data = (train_x, train_y))
 score = model.evaluate(test_x, test_y, verbose = 0)
